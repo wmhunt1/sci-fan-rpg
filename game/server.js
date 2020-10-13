daa8c50542b61129649e24a3e803d1fe21e06e1d
Matt Hunt <wmatthunt@gmail.com>
2020-10-13 09:14:02 -0400

    basic setup

---
+++ game/server.js
@@ -0,0 +1,24 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+// require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+// =============================================================
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
